642eae6c6d6837314581a31fc565892020b04874
Cale Teeter <caleteeter@users.noreply.github.com>
2018-04-28 23:12:41 -0400

    Removed specific server info

--- walkthrough1/truffle.js
+++ walkthrough1/truffle.js
@@ -6,20 +6,20 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     azure_node0: {
-      host: "40.70.9.93",
+      host: "",
       port: 8501,
       network_id: "1010",
       gasPrice: 0,
       gas: 500000,
-      from: "0x1d1907b41ea11ffb9881de78872b8cd350e4c799"
+      from: ""
     },
     azure_node1: {
-      host: "40.70.9.93",
+      host: "",
       port: 8502,
       network_id: "1010",
       gasPrice: 0,
       gas: 500000,
-      from: "0xef68c0e6c047e910305411bdeb60879ccbce7a5a"
+      from: ""
     }
   }
 };
